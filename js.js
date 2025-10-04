d0228fdfc4b4c03787bda9045d3191c5986b2470
Mansour-Alaswani <mansoe19999@gmail.com>
2025-10-04 22:03:21 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+
+
+let but = document.getElementById('but');
+let in1 = document.getElementById('in1');
+let in2 = document.getElementById('in2');
+let b3 = document.getElementById('b3');
+let a3 = document.getElementById('a3');
+
+
+
+
+        but.onclick = function(){
+            let mans = in1.value * 0.039 ;
+            let man = in2.value ;
+            if(man == 45){
+                b3.innerHTML = 'الحمل المسموح للعمل'  +"<br>" +( Math.pow(mans,2) * 45 / 5).toFixed(2) + ' Ton';
+                a3.innerHTML = 'الحمل القطع للواير'  +"<br>" + (Math.pow(mans,2) * 45).toFixed(2)  + ' Ton';
+                a3.style.display = 'block';
+                a3.style.background = 'red';
+                b3.style.display = 'block';
+                b3.style.background = '#ffe600c2';
+            }else if(man == 42){
+                b3.innerHTML = 'الحمل المسموح للعمل'  +"<br>" + Math.pow(mans,2) * 42 / 5 + ' Ton' ;
+                a3.innerHTML = 'الحمل القطع للواير'  +"<br>" + Math.pow(mans,2) * 42  + ' Ton';
+                a3.style.display = 'block';
+                a3.style.background = 'red';
+                b3.style.display = 'block';
+                b3.style.background = '#ffe600c2';
+            }
+            
+        }
